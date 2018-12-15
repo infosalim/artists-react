97a191f7b72bbdf1a575c4500a825fb360bcb141
Salim Hossain <salimh5757@gmail.com>
2018-12-16 04:02:58 +0600

    created bannder

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Banner from './Banner';
 
 class Home extends Component{
 
     render(){
         return (
             <div>
-                Home
+                <Banner/>
             </div>
         );
     }
